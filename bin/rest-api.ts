e72cbcb94206ec11f637937bfdac1384a9e8e959
dlaw4608 <dlawton032@gmail.com>
2024-11-11 13:35:18 +0000

    Refactored auth stack into custom constructs

--- bin/rest-api.ts
+++ bin/rest-api.ts
@@ -2,8 +2,8 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { RestAPIStack } from "../lib/rest-api-stack";
-import { AuthApiStack } from '../lib/auth-api-stack';
+import { AuthAppStack } from '../lib/auth-app-stack';
 
 const app = new cdk.App();
 new RestAPIStack(app, "RestAPIStack", { env: { region: "eu-west-1" } });
-new AuthApiStack(app, "AuthApiStack", { env: { region: "eu-west-1" } });
+new AuthAppStack(app, "AuthAppStack", { env: { region: "eu-west-1" } });
